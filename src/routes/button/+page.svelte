659754786ab8e985509eab738228b4313197f208
Scott Andrew Bedis <bedisscottandrew@gmail.com>
2024-06-05 16:16:55 +0800

    added simple card

--- src/routes/button/+page.svelte
+++ src/routes/button/+page.svelte
@@ -1,4 +1,11 @@
-<button
-	class="px-6 py-2 font-sans font-semibold text-white transition-all duration-100 bg-black rounded hover:bg-black/75"
-	>Button</button
->
+<div class="flex items-center justify-center gap-4">
+	<button
+		class="px-6 py-2 font-sans font-semibold text-white transition-all duration-300 bg-black rounded hover:bg-black/75"
+		>Button</button
+	>
+
+	<button
+		class="px-6 py-2 font-sans font-semibold transition-all duration-300 border border-black rounded hover:bg-black hover:text-white"
+		>Button</button
+	>
+</div>
---
+++ src/routes/card/+page.svelte
@@ -0,0 +1,15 @@
+<div class="flex flex-col px-4 py-2 border border-black rounded h-3/4 w-72">
+	<h1 class="text-lg font-bold">User Information</h1>
+	<div class="flex flex-col flex-1 gap-3 px-4 pt-6">
+		<p class="text-xs">Firstname: <span class="font-semibold">Scott Andrew</span></p>
+		<p class="text-xs">Lastname: <span class="font-semibold">Bedis</span></p>
+		<p class="text-xs">Age: <span class="font-semibold">22</span></p>
+		<p class="text-xs">Occupation: <span class="font-semibold">Software Developer</span></p>
+		<p class="text-xs">Email: <span class="font-semibold">bedisscottandrew@gmail.com</span></p>
+		<p class="text-xs">Contact: <span class="font-semibold">09082091358</span></p>
+		<p class="text-xs">Address: <span class="font-semibold">Bonga, Bacacay, Albay</span></p>
+	</div>
+	<div class="flex items-center justify-center">
+		<button class="px-4 py-1 text-xs font-medium border border-black rounded">Save</button>
+	</div>
+</div>
