9953f7b38c546987d5773a7de78402d243469603
Scott Andrew Bedis <bedisscottandrew@gmail.com>
2024-06-10 22:27:54 +0800

    added tooltip

---
+++ src/routes/tooltip/+page.svelte
@@ -0,0 +1,8 @@
+<div class="relative px-4 py-2 border rounded group">
+	<h1 class="font-sans font-medium tracking-tight">Hover me</h1>
+	<div
+		class="absolute inset-x-0 p-1 text-center transition-all duration-500 border rounded opacity-0 -top-8 group-hover:opacity-100"
+	>
+		<p class="text-xs">I am a tooltip</p>
+	</div>
+</div>
