d2547c46d8641b437c2ad394dd0e55871ff2c0f0
Scott Andrew Bedis <bedisscottandrew@gmail.com>
2024-06-11 23:41:29 +0800

    added radio button

---
+++ src/routes/radiobutton/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex items-center">
+	<input type="radio" id="radio1" name="radio" class="radio-custom" />
+	<label for="radio1" class="ml-2 text-gray-700">Option 1</label>
+</div>
+
+<div class="flex items-center mt-2">
+	<input type="radio" id="radio2" name="radio" class="radio-custom" />
+	<label for="radio2" class="ml-2 text-gray-700">Option 2</label>
+</div>
